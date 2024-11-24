eca26a1341402982daa935f5634185dc0271e16b
Vani Aggarwal <vaniaggarwal96@gmail.com>
2024-11-24 22:55:07 +0530

    Created one box on the button command ; As many boxes as the number typed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('.gridDimensions');
+const gridBox = document.querySelector('.grid');
 
 
 let size = 0;
@@ -6,11 +7,44 @@ let size = 0;
 const displayPrompt = () => {
    size = prompt("Enter the grid length/width ");
    console.log(size) 
-   if (size > 2 && size < 100){}
+   if (size >= 2 && size <= 100){}
    else{
     prompt("invalid input,enter the grid length/width again")
    }
    return size;
 };
 
-btn.addEventListener('click' , displayPrompt);
+// create one box
+
+
+
+
+
+
+
+
+
+
+
+
+// pass it through for function with size as the max
+
+function finalBoxNumber (){
+    for(let i = 0 ; i < size ; i++){
+        const draw = () => {
+            const box = document.createElement("canvas");
+            box.classList.add("box");
+            box.style.border = "1px solid black";
+            gridBox.appendChild(box);
+        }
+        draw()
+    }
+}
+
+function allTogether(){
+    displayPrompt();
+    finalBoxNumber();
+}
+
+// it appears as the button is clicked
+btn.addEventListener('click' , allTogether);
